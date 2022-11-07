cecf4aa5f69e9cb399e7e3752ee177cfa105d850
John Zhuoan Lu <jlu393@rg-login.crnch.gatech.edu>
2022-11-07 12:58:55 -0500

    continue runtime function porting

--- runtime/src/cudaRuntimeImpl.cpp
+++ runtime/src/cudaRuntimeImpl.cpp
@@ -11,6 +11,18 @@
 
 // https://docs.nvidia.com/cuda/cuda-runtime-api/structcudaDeviceProp.html
 
+cudaError_t cudaDeviceRest() {
+  HIP_CHECK(hipDeviceReset());
+}
+
+cudaError_t cudaGetLastError() {
+  HIP_CHECK(hipGetLastError());
+}
+
+cudaError_t cudaPeekAtLastError() {
+  HIP_CHECK(hipPeekAtLastError());
+}
+
 cudaError_t cudaGetDevice(int *devPtr) { 
   printf("insideGetDevice\n");
   HIP_CHECK(hipGetDevice(devPtr));
@@ -28,3 +40,27 @@ cudaError_t cudaGetDeviceProperties (cudaDeviceProp* prop, int  device)
   printf("Device clockRate: %d \n", prop->clockRate);
 }
 
+cudaError_t cudaMalloc (void ** devPtr, size_t size) {
+  printf("insideCudaMalloc\n");
+  HIP_CHECK(hipMalloc(ptr, size));
+}
+
+cudaError_t cudaFree (void* ptr) {
+  HIP_CHECK(hipFree(ptr));
+}
+
+cudaError_t cudaHostAlloc (void** ptr, size_t size, unsigned int flag) {
+  HIP_CHECK(hipHostMalloc(ptr, size, flag));
+}
+
+
+
+cudaError_t cudaMemcpy (void* dst, const void* src, size_t count, cudaMemcpyKind kind) {
+  printf("insideCudaMemcpy\n");
+  HIP_CHECK(hipMemCpy(dst, src, count, (hipMemcpyKind)kind))
+}
+
+cudaError_t cudaLaunchKernel (const void* func, dim3 gridDim, dim3 blockDim, void** args, size_t sharedMem, cudaStream_t stream) {
+  printf("insideCudaLaunchKernel\n");
+  HIP_CHECK(hipLaunchKernel(func, gridDim, blockDim, args, sharedMem, (hipStream_t) stream));
+}
