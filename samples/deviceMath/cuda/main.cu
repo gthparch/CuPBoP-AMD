df8f5a09b8f252e57f7dee8c3d729f1b19b4770f
Marcus Zhou <7273813+SuperMarcus@users.noreply.github.com>
2023-02-07 12:33:59 -0500

    Properly link against device libs

--- samples/deviceMath/cuda/main.cu
+++ samples/deviceMath/cuda/main.cu
@@ -1,28 +1,49 @@
+#include <cuda.h>
 #include "cuda_runtime.h"
 #include "stdio.h"
 #include "math.h"
 #include <cmath>
 
-#define NUM_TEST_CALLS 2
+#define NUM_TEST_CALLS 26
 
-// __device__ float wrap_sin(float input) {
-//     return sin(input);
-// }
-
-// __device__ float wrap_cos(float input) {
-//     return cos(input);
-// }
-
-#define TEST_SEQ(in, out) \
-    out[0] = sin(in[0]); \
-    out[1] = cos(in[1]);
+#define TEST_SEQ(in, out, counter) \
+    out[counter] = (int) ::lgamma(10.0f); counter += 1; \
+    out[counter] = (int) ::exp(1.0f); counter += 1; \
+    out[counter] = (int) ::log(1.0f); counter += 1; \
+    out[counter] = (int) ::log10(1000.0f); counter += 1; \
+    out[counter] = (int) ::log1p(0.0f); counter += 1; \
+    out[counter] = (int) ::log2(1000.0f); counter += 1; \
+    out[counter] = (int) ::expm1(1.0f); counter += 1; \
+    out[counter] = (int) ::cos(0.0f); counter += 1; \
+    out[counter] = (int) ::sin(0.0f); counter += 1; \
+    out[counter] = (int) ::sqrt(100.0f); counter += 1; \
+    out[counter] = (int) ::ceil(2.4f); counter += 1; \
+    out[counter] = (int) ::floor(2.7f); counter += 1; \
+    out[counter] = (int) ::trunc(2.7f); counter += 1; \
+    out[counter] = (int) ::cosh(1.0f); counter += 1; \
+    out[counter] = (int) ::acosh(1.0f); counter += 1; \
+    out[counter] = (int) ::asin(1.0f); counter += 1; \
+    out[counter] = (int) ::sinh(1.0f); counter += 1; \
+    out[counter] = (int) ::asinh(1.0f); counter += 1; \
+    out[counter] = (int) ::tan(0.0f); counter += 1; \
+    out[counter] = (int) ::atan(1.0f); counter += 1; \
+    out[counter] = (int) ::tanh(1.0f); counter += 1; \
+    out[counter] = (int) ::erf(10.0f); counter += 1; \
+    out[counter] = (int) ::erfc(10.0f); counter += 1; \
+    out[counter] = (int) ::round(2.3f); counter += 1; \
+    out[counter] = (int) ::pow(2.0f, 10.0f); counter += 1; \
+    out[counter] = (int) ::atan2(7.0f, 0.0f); counter += 1; \
+    /* out[counter] = (int) ::isnan(0.0f); counter += 1; */ \
+    /* out[counter] = (int) ::isinf(0.0f); counter += 1; */
 
 void test_math_host(float *in, float *out) {
-    TEST_SEQ(in, out)
+    int counter = 0;
+    TEST_SEQ(in, out, counter)
 }
 
 __global__ void test_math(float *in, float *out) {
-    TEST_SEQ(in, out)
+    int counter = 0;
+    TEST_SEQ(in, out, counter)
 }
 
 void check_is_close(int cs, float result, float ref, float error = 1e-6) {
