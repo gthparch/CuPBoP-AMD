69efa8e52c2946c76e4e8bca18a7a82c7b1a2585
Marcus Zhou <7273813+SuperMarcus@users.noreply.github.com>
2023-02-28 13:10:26 -0500

    Update Makefile for fair comparison

--- benchmarks/rodinia/hotspot3D/run.sh
+++ benchmarks/rodinia/hotspot3D/run.sh
@@ -5,8 +5,10 @@ RODINIA_PATH=`realpath ..`
 
 make
 
+# export AMD_LOG_LEVEL=4
+
 echo "[*] Running CuPBoP version..."
-AMD_LOG_LEVEL=2 LD_LIBRARY_PATH="$AMDCUDA_DIR/build/runtime:$LD_LIBRARY_PATH" ./hotspot3D.cupbop \
+LD_LIBRARY_PATH="$AMDCUDA_DIR/build/runtime:$LD_LIBRARY_PATH" ./hotspot3D.cupbop \
     512 8 100 \
     "$RODINIA_PATH/rodinia-data/hotspot3D/power_512x8" \
     "$RODINIA_PATH/rodinia-data/hotspot3D/temp_512x8" \
