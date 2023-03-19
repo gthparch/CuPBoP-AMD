601c233114f13fefa0de0257fe744e64e74bfae8
Marcus Zhou <7273813+SuperMarcus@users.noreply.github.com>
2023-03-19 13:26:24 -0400

    Add nw benchmark

---
+++ benchmarks/rodinia/nw/needle.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef RD_WG_SIZE_0_0
+#define BLOCK_SIZE RD_WG_SIZE_0_0
+#elif defined(RD_WG_SIZE_0)
+#define BLOCK_SIZE RD_WG_SIZE_0
+#elif defined(RD_WG_SIZE)
+#define BLOCK_SIZE RD_WG_SIZE
+#else
+#define BLOCK_SIZE 16
+#endif
+// #define TRACE
