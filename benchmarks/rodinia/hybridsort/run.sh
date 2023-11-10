af9731a7a90f7b9068759bf7bc5b11c3d88d5b21
Jun Chen <junchen1511@gmail.com>
2023-11-10 01:20:29 -0500

    add p3hpc commit

--- benchmarks/rodinia/hybridsort/run.sh
+++ benchmarks/rodinia/hybridsort/run.sh
@@ -8,15 +8,15 @@ SAVE_OUTPUTS=0
 make
 
 echo "[*] Running CuPBoP version with random values..."
-time ./hybridsort.cupbop rodinia-data/hybridsort/500000.txt
+time ./hybridsort.cupbop ../rodinia-data/hybridsort/500000.txt
 
 echo "[*] Running HIPIFY version..."
-time ./hybridsort.cupbop rodinia-data/hybridsort/500000.txt
+time ./hybridsort.cupbop ../rodinia-data/hybridsort/500000.txt
 
-if [[ "$SAVE_OUTPUTS" -eq 0 ]]; then
-    echo "[*] Cleaning up..."
-    make clean
-fi
+# if [[ "$SAVE_OUTPUTS" -eq 0 ]]; then
+#     echo "[*] Cleaning up..."
+#     make clean
+# fi
 
 # ./hybridsort.cupbop r
 # ./hybridsort.cupbop /nethome/jchen706/coding/code2/amdcuda/benchmarks/rodinia/rodinia-data/hybridsort/t.txt > z1.txt
--- benchmarks/rodinia/leukocyte/run.sh
+++ benchmarks/rodinia/leukocyte/run.sh
@@ -9,5 +9,5 @@ make
 echo "[*] Running CuPBoP version..."
 time ./leukocyte.cupbop "$RODINIA_PATH/rodinia-data/leukocyte/testfile.avi" 5
 
-echo "[*] Running HIPIFY version..."
-time ./leukocyte.hipify "$RODINIA_PATH/rodinia-data/leukocyte/testfile.avi" 5
+# echo "[*] Running HIPIFY version..."
+# time ./leukocyte.hipify "$RODINIA_PATH/rodinia-data/leukocyte/testfile.avi" 5
