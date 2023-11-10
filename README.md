0c1aa2b4487df347c1ee3e2c7bc344dcf897ac29
Marcus Zhou <7273813+SuperMarcus@users.noreply.github.com>
2023-11-10 17:29:36 -0500

    Add apache 2.0 license

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # CuPBoP-AMD: Extending CUDA to AMD Platforms
 
+> Copyright © 2023
+>
+> Licensed under the Apache License, Version 2.0 (the "License");
+> you may not use this file except in compliance with the License.
+> You may obtain a copy of the License at
+>
+> http://www.apache.org/licenses/LICENSE-2.0
+>
+> Unless required by applicable law or agreed to in writing, software
+> distributed under the License is distributed on an "AS IS" BASIS,
+> WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+> See the License for the specific language governing permissions and
+> limitations under the License.
+
 ## Introduction
 
 CuPBoP-AMD (Extending CUDA to AMD Platforms) is a extension of the framework of CuPBoP following similar architecture. However,  CuPBoP-AMD has its over version of kernerl and host translators and runtime implementation.
--- compilation/kernel/src/DeviceVectorArgPass.cpp
+++ compilation/kernel/src/DeviceVectorArgPass.cpp
@@ -1,10 +1,16 @@
-/*
-
-  Walk through device and kernel functions and redefine CUDA vector types to 
-  HIP vectors 
-
-
-*/
+// Copyright © 2023
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include <iostream>
 #include <memory>
@@ -28,6 +34,8 @@
 
 using namespace llvm;
 using namespace cupbop::amd::passes;
+
+// Walk through device and kernel functions and redefine CUDA vector types to HIP vectors
 
 VectorArgPass::VectorArgPass() : ModulePass(ID) {}
 
